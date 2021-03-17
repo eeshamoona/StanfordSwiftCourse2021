71f7c66fab3a4d527ff75f18cee7b4510fef9b10
eeshamoona <eeshamoona@gmail.com>
2021-03-17 00:06:45 -0500

    Finished Lecture 1 + tried some of my own coding

--- StanfordSwiftCourse2021/ContentView.swift
+++ StanfordSwiftCourse2021/ContentView.swift
@@ -7,10 +7,61 @@
 
 import SwiftUI
 
+enum State {
+    case info
+    case threwAway
+    case used
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return VStack( spacing: 15) {
+            Text("Soon to Expire").font(.largeTitle)
+            ForEach(0..<4) { index in
+                ExpireCard(stateOfCard: State.info)
+            } 
+            MenuBar()
+        }.padding().foregroundColor(Color.black).font(Font.title)
+    }
+}
+
+struct ExpireCard: View {
+    var stateOfCard: State
+    var body: some View {
+        switch stateOfCard {
+        case .info:
+            return ZStack() {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("Item 1 Due in 4 Days ")
+           }
+        case .threwAway:
+            return ZStack() {
+               RoundedRectangle(cornerRadius: 10.0).fill(Color.red)
+               RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+               Text("X")
+           }
+        case .used:
+            return ZStack() {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.green)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("✔︎")
+            }
+        }
+    }
+}
+
+struct MenuBar: View {
+    var body: some View {
+        return HStack( spacing: 15) {
+            Button {
+                print("Button pressed")
+            } label: {
+                Text("Add Item")
+                    .padding(20).foregroundColor(.blue)
+            }
+            .contentShape(Rectangle())
+        }
     }
 }
 
