a08b782e652744dd0c5793672af630ab08e48303
eeshamoona <eeshamoona@gmail.com>
2021-03-16 19:19:14 -0500

    Initial Commit

---
+++ StanfordSwiftCourse2021/StanfordSwiftCourse2021App.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordSwiftCourse2021App.swift
+//  StanfordSwiftCourse2021
+//
+//  Created by Eesha Moona on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct StanfordSwiftCourse2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
